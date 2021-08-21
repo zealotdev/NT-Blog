0f54475c0cd0c19cb5777502a4b6a8706030726f
zealotdev <g33ky.me@pm.me>
2021-08-21 22:44:16 +0300

    Added About Page and route

--- pages/about.js
+++ pages/about.js
@@ -10,7 +10,7 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>Home Page</h1>
+        <h1 className={styles.title}>About Page</h1>
       </main>
     </div>
   );
