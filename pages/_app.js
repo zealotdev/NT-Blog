972001e51f05fd83367d122a6b047c810edf2b10
zealotdev <g33ky.me@pm.me>
2021-08-22 02:03:40 +0300

    Overlay Layout

--- pages/_app.js
+++ pages/_app.js
@@ -4,21 +4,31 @@ import '../styles/globals.css';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div className="mx-auto ">
-      <nav>
-        <ul>
-          <li>
-            <Link href="/">
-              <a>Home</a>
-            </Link>
-          </li>
-          <li>
-            <Link href="/about">
-              <a>About</a>
-            </Link>
-          </li>
-        </ul>
-      </nav>
+    <div className="mx-auto w-9/12">
+      <header className="mb-12 py-4 flex justify-between items-baseline subpixel-antialiased border-b-2 border-blue-50">
+        <div>
+          <Link href="/">
+            <a className=" text-blue-600 text-6xl font-bold line-through">
+              Blog NT
+            </a>
+          </Link>
+        </div>
+        <nav className="mr-0">
+          <ul className="flex justify-between space-x-4 ">
+            <li>
+              <Link href="/">
+                <a>Home</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/about">
+                <a>About</a>
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </header>
+
       <Component {...pageProps} />
     </div>
   );
